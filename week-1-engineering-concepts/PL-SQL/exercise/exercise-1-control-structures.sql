7f04674aaf6893e22b89c52d713ea33456c8da43
Adwait Patel <145041038+AdwaitPatel@users.noreply.github.com>
2026-06-25 23:28:03 +0530

    exercise 1 control structures is done

--- week-1-engineering-concepts/PL-SQL/exercise/exercise-1-control-structures.sql
+++ week-1-engineering-concepts/PL-SQL/exercise/exercise-1-control-structures.sql
@@ -28,6 +28,26 @@ Question: Write a PL/SQL block that iterates through all customers and sets a fl
 to TRUE for those with a balance over $10,000. 
 */
 
+-- add a IsVIP column
+ALTER TABLE Customers
+ADD IsVIP CHAR(1) DEFAULT 'N';
+
+DECLARE
+BEGIN
+    FOR customer IN (
+        SELECT *
+        FROM Customers
+    )
+    LOOP
+        IF customer.Balance > 10000 THEN
+            UPDATE Customers
+            SET IsVIP = 'Y'
+            WHERE CustomerID = customer.CustomerID;
+        END IF;
+    END LOOP;
+END;
+/
+
 
 /*
 Scenario 3: The bank wants to send reminders to customers whose loans are due within the 
@@ -36,6 +56,27 @@ Question: Write a PL/SQL block that fetches all loans due in the next 30 days an
 reminder message for each customer. 
 */
 
+DECLARE
+BEGIN
+    FOR loan_rec IN (
+        SELECT 
+            c.Name,
+            l.LoanID,
+            l.EndDate
+        FROM Loans l
+        JOIN Customers c
+            ON l.CustomerID = c.CustomerID
+        WHERE l.EndDate BETWEEN SYSDATE AND SYSDATE + 30
+    )
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(
+            'Reminder: Dear ' || loan_rec.Name ||
+            ', your Loan ID ' || loan_rec.LoanID ||
+            ' is due on ' || TO_CHAR(loan_rec.EndDate, 'DD-MON-YYYY')
+        );
+    END LOOP;
+END;
+/
 
 
 
