63aced4417b9de81fd7d550d2f0df9116a9dd11b
Adwait Patel <145041038+AdwaitPatel@users.noreply.github.com>
2026-06-25 22:45:45 +0530

    plsql ex1 q1 done

--- week-1-engineering-concepts/PL-SQL/exercise/exercise-1-control-structures.sql
+++ week-1-engineering-concepts/PL-SQL/exercise/exercise-1-control-structures.sql
@@ -1,8 +1,25 @@
 /*
-Scenario 1: The bank wants to apply a discount to loan interest rates for customers above 60 years old. 
+Scenario 1: The bank wants to apply a discount to loan interest rates for customers above 60 years old.
 Question: Write a PL/SQL block that loops through all customers, checks their age, and if they are above 60, apply a 1% discount to their current loan interest rates. 
 */
 
+DECLARE
+    age NUMBER;
+BEGIN
+    FOR customer IN (
+        SELECT *
+        FROM Customers
+    )
+    LOOP
+        age := TRUNC(MONTHS_BETWEEN(SYSDATE, customer.DOB) / 12);
+        IF age > 60 THEN
+            UPDATE LOANS
+            SET InterestRate = InterestRate - 1
+            WHERE CustomerID = customer.CustomerID;
+        END IF;
+    END LOOP;
+END;
+/
 
 
 /*
