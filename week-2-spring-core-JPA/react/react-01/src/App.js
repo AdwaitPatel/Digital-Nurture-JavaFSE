8131f3ec479b2d34a93d14450709fb84dafc4a1e
Adwait Patel <145041038+AdwaitPatel@users.noreply.github.com>
2026-07-06 09:17:31 +0530

    react basics and props

--- week-2-spring-core-JPA/react/react-01/src/App.js
+++ week-2-spring-core-JPA/react/react-01/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import DeveloperBios from './components/DeveloperBios';
+
+const devInfo = {
+	firstName: "Adwait",
+	lastName: "Patel",
+	favoriteLanguage: "JS",
+	yearStarted: 2025
+}
+
+class ClassComponent extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			header: "header from state",
+			content: "content from state"
+		}
+	}
+	render() {
+		return (
+			<div>
+				<h1>{this.state.header}</h1>
+				<h2>{this.state.content}</h2>
+			</div>
+		)
+	}
+}
+
+class Developer {
+	constructor(id, firstName, lastName, favoriteLanguage, yearStarted) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.favoriteLanguage = favoriteLanguage;
+		this.yearStarted = yearStarted;
+	}
+}
+
+class DisplayBios extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			developers: [
+				new Developer(1, "adwait", "patel", "js", 2025),
+				new Developer(2, "kunal", "kushwah", "java", 2019)
+			]
+		}
+
+	}
+
+	render() {
+		return (
+			this.state.developers.map(developer => <DeveloperBios {...developer} />)
+		)
+	}
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div class="d-flex flex-column justify-content-center align-items-center" style={{ height: "100vh" }}>
+			{/* <DeveloperBios {...devInfo} /> */}
+			{/* <ClassComponent /> */}
+			<DisplayBios />
+		</div>
+	);
 }
 
 export default App;
